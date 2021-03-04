30e209856e37b133d79f50d458cabeb00334db8f
Md. Shariful Islam <abinsu7@gmail.com>
2021-03-05 00:11:30 +0600

    One code is added from ZHO sir slides

---
+++ theroyLec03/WhileLoopProblemByZHOLec03.java
@@ -0,0 +1,14 @@
+package theroyLec03;
+
+public class WhileLoopProblemByZHOLec03 {
+
+	public static void main(String[] args) {
+		int i =100, j= 200;
+		
+		while(++i < --j);
+		System.out.println("Midpoint: " + i);
+			
+
+	}
+
+}
