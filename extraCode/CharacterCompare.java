f163cace70b09017a1343cc6e410e55f99da5fde
Md. Shariful Islam <abinsu7@gmail.com>
2021-03-08 11:39:04 +0600

    CharacterCompare code is added

---
+++ extraCode/CharacterCompare.java
@@ -0,0 +1,13 @@
+public class CharacterCompare {  
+	public static void main(String[] args) {  
+		char ch1= 'A';  
+		char ch2= 'B';   
+    		
+		int comp = Character.compare(ch1, ch2);   // Compare the ch1 to ch2
+    		if (comp > 0) 
+    			System.out.println(ch1 + " is greater than " + ch2);  
+  		else  
+			System.out.println(ch1 + " is less than " + ch2);    
+ 
+   	}  
+}                                                                                                                                                                                                                                                                                       ----------------------------------------------------------------------------------------------------------------------------------
