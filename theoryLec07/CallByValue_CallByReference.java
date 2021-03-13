cb7531cefc6593228f455ced227885a4e8133b7a
Md. Shariful Islam <abinsu7@gmail.com>
2021-03-14 01:23:38 +0600

    Class 09. Today's code is added [13/03/2021] -> Call by value and Call by reference

---
+++ theoryLec07/CallByValue_CallByReference.java
@@ -0,0 +1,48 @@
+package theoryLec07;
+
+import java.util.Scanner;
+
+public class CallByValue_CallByReference {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		// Call By Value
+		//int number = 20;
+		//System.out.println("Befor calling Method: " + number);  // 20
+		
+		//int result = CallByValue(number);
+		//System.out.println("Calling Method: " + result);  //  40
+		
+		//System.out.println("Befor calling Method: " + number); // 20
+		
+		
+		// Call By Reference
+		int a[] = {10, 20, 30};
+		System.out.println("Value of a: ");
+		for (int i = 0; i < a.length; i++) {
+			System.out.print(a[i] + " ");
+		}
+		
+		int b[] = CallByReference(a);  // Address of a = 2345A
+		System.out.println("\nValue of b: ");
+		for (int i = 0; i < b.length; i++) {
+			System.out.print(b[i] + " ");
+		}
+		
+		System.out.println("\n\nAgain Value of a: ");
+		for (int i = 0; i < a.length; i++) {
+			System.out.print(a[i] + " ");
+		}
+	
+	}
+	public static int CallByValue(int value) {
+		return  value + 20;
+	}
+	
+	public static int[] CallByReference(int v[]) {
+		for (int i = 0; i < v.length; i++) {
+			v[i] = v[i] + 5 ;
+		}
+		return v;
+	}
+}
