d1a8a8eaa5603d413cc1f5802c88f041244a2bb7
Md. Shariful Islam <abinsu7@gmail.com>
2021-03-13 20:36:51 +0600

    Some new line are added for review class

--- theoryLec01/IncrementANDDecrement.java
+++ theoryLec01/IncrementANDDecrement.java
@@ -3,7 +3,12 @@ package theoryLec01;
 public class IncrementANDDecrement {
 
 	public static void main(String[] args) {
-		// Increment & Decrement 
+		int t = 5;
+		int s = t++;
+		System.out.println("S:" +s  + "\nT:" + t );  // s=6 t=5
+		
+		
+		// Increment & Decrement
 		int a = 10;
 		a = a + 5;
 		a = a - 4;
@@ -38,41 +43,18 @@ public class IncrementANDDecrement {
 		r = --r + 2;  // r = 21  r = 19
 		q =  ++p + --r; // q = 21 + 20 = 41    p=21
 		
-		System.out.println("P: " + p);
-		System.out.println("Q: " + q);
-		System.out.println("R: " + r);
+		System.out.println("P: " + p);  // 21
+		System.out.println("Q: " + q);  // 41
+		System.out.println("R: " + r);  // 20
+		
+		// P: 20  Q: 43 R: 22
 		
 		
 		
 		int xyz = a++ + ++a;   //<----// right to left(Unary post-fix and  pre-fix)
-		System.out.println(xyz);
-		System.out.println(a);
+		//System.out.println(xyz);
+		//System.out.println(a);
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-
 	}
 
 }
