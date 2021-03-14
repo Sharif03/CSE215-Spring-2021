2d34d620a25fcc81f06e3f9f0263b535a549d718
Md. Shariful Islam <abinsu7@gmail.com>
2021-03-15 00:43:56 +0600

    Codes are same as like before one. No major change

--- theoryLec07/CallByValue_CallByReference.java
+++ theoryLec07/CallByValue_CallByReference.java
@@ -7,39 +7,39 @@ public class CallByValue_CallByReference {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		// Call By Value
-		//int number = 20;
-		//System.out.println("Befor calling Method: " + number);  // 20
+		int number = 20;
+		System.out.println("Befor calling Method: " + number);  // 20
 		
-		//int result = CallByValue(number);
-		//System.out.println("Calling Method: " + result);  //  40
+		int result = CallByValue(number);
+		System.out.println("Calling Method: " + result);  //  40
 		
-		//System.out.println("Befor calling Method: " + number); // 20
+		System.out.println("Befor calling Method: " + number); // 20
 		
 		
 		// Call By Reference
-		int a[] = {10, 20, 30};
+		int intArray[] = {10, 20, 30};
 		System.out.println("Value of a: ");
-		for (int i = 0; i < a.length; i++) {
-			System.out.print(a[i] + " ");
+		for (int i = 0; i < intArray.length; i++) {
+			System.out.print(intArray[i] + " ");
 		}
 		
-		int b[] = CallByReference(a);  // Address of a = 2345A
+		int b[] = CallByReference(intArray);  // Address of a = 2345A
 		System.out.println("\nValue of b: ");
 		for (int i = 0; i < b.length; i++) {
 			System.out.print(b[i] + " ");
 		}
 		
 		System.out.println("\n\nAgain Value of a: ");
-		for (int i = 0; i < a.length; i++) {
-			System.out.print(a[i] + " ");
+		for (int i = 0; i < intArray.length; i++) {
+			System.out.print(intArray[i] + " ");
 		}
-	
 	}
+	
 	public static int CallByValue(int value) {
 		return  value + 20;
 	}
 	
-	public static int[] CallByReference(int v[]) {
+	public static int[] CallByReference(int v[]) {  // address of intArray
 		for (int i = 0; i < v.length; i++) {
 			v[i] = v[i] + 5 ;
 		}
